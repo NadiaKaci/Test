5aaa7b1528154921801105fc5bd25d6584dbb503
NadiaKaci <nadia.kacin@gmail.com>
2026-01-15 14:26:23 -0500

    2eme commit: creation du 1er trinagle exercice1-avec-classe.py

--- exercice1-avec-classe.py
+++ exercice1-avec-classe.py
@@ -3,3 +3,8 @@
 class Triangle:
     def __init__(self, n):
         self.n = n
+
+    # creation du premier triangle
+    def ligne_inversee(self, i):
+        return ' ' * (self.n - i) + '*' * i
+
