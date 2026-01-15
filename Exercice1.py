852ff7936df0e65f837b670a18a25adf9c6dcd4f
NadiaKaci <nadia.kacin@gmail.com>
2026-01-15 14:31:19 -0500

    5eme commit: creation Programme principal exercice1-avec-classe.py

--- Exercice1.py
+++ Exercice1.py
@@ -10,6 +10,6 @@ for i in range(1, n + 1):
     espace = ' '
     # deuxième triangle normal
     ligne2 = '*' * i
-    # afficher la ligne complète
+    # afficher
     print(ligne1 + espace + ligne2)
 
