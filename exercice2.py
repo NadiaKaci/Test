49d5301b8610c0808ab10eef718d53dadae9d9c2
NadiaKaci <nadia.kacin@gmail.com>
2026-01-15 15:17:20 -0500

    4eme commit: creation de Button exercice2.py

--- exercice2.py
+++ exercice2.py
@@ -1,5 +1,5 @@
 
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QLabel, QPushButton
 
 # créer l'application:
 app = QApplication([])
@@ -15,9 +15,15 @@ window.setLayout(grid)
 nombre_input = QLineEdit(window)
 grid.addWidget(nombre_input, 1, 1)
 grid.addWidget(QLabel("Voici le double: "), 2, 0)
-
+#Qlabel:
 grid.addWidget(QLabel("Enter la valeur de N: "), 1, 0)
 grid.addWidget(QLabel("Voici le double: "), 2, 0)
+
+#creation des button:
+btn1 = QPushButton("valider l'operation")
+grid.addWidget(btn1, 3, 1)
+
+
 
 window.show()
 app.exec()
