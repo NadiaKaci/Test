bd5b7e486cbd6fe5f12259221e08d16020ec6fc7
NadiaKaci <nadia.kacin@gmail.com>
2026-01-15 14:12:35 -0500

    3eme commit: creation de l'espace entre les triangles Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -6,3 +6,5 @@ n = int(input("Entrez un entier : "))
 for i in range(1, n + 1):
     # premier triangle inversé
     ligne1 = ' ' * (n - i) + '*' * i
+    # céer espace entre les triangles
+    espace = ' '
