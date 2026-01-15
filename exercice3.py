7744897b8775c4eb1f6b9ff0ad7c427d5e2044f9
NadiaKaci <nadia.kacin@gmail.com>
2026-01-15 16:26:29 -0500

    1er commit: #Creation de la classe Outils, les fonctions: saisir, min , max exercice3.py

--- exercice3.py
+++ exercice3.py
@@ -3,3 +3,23 @@
 class Outils:
     def __init__(self):
         self.nombres = []
+
+    def saisir(self):
+        self.nombres = []
+        for i in range(10):
+            n = int(input("Entrez un entier : "))
+            self.nombres.append(n)
+
+    def min(self):
+        minimum = self.nombres[0]
+        for n in self.nombres:
+            if n < minimum:
+                minimum = n
+        return minimum
+
+    def max(self):
+        maximum = self.nombres[0]
+        for n in self.nombres:
+            if n > maximum:
+                maximum = n
+        return maximum
